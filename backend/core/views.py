a6f2736b820fc215ad42225addb494031131b10e
Koded0214h <coder0214h@gmail.com>
2025-08-10 00:35:04 +0100

    issue: media files handling

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,6 +1,6 @@
 import mimetypes
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.models import User
 from django.template.loader import render_to_string
 from django.http import FileResponse, Http404
